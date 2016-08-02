59cc3ede3ccb933125f29774055c3a4e9711ee72
yugq <yugq@fenqi.im>
2016-08-02 13:09:18 +0800

    change default port to 8080

--- startDocker.sh
+++ startDocker.sh
@@ -1,1 +1,1 @@
-docker run -d --name DouD --volumes-from jenkinsDV -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/usr/bin/docker -p 8082:8080 -p 50005:50000 flasheryu/myjenk:201607261202
+docker run -d --name DouD --volumes-from jenkinsDV -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/usr/bin/docker -p 8080:8080 -p 50000:50000 flasheryu/jenkins
